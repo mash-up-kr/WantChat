6732c0b90978bde6cfef79114b83071ec4d1daf7
jonghyeonkim <whdgus8219@naver.com>
2020-12-19 16:01:03 +0900

    init commit

---
+++ src/main/java/com/mashup/wantchatmainapi/WantChatMainApiApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.wantchatmainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantChatMainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantChatMainApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mashup/wantchatmainapi/WantChatMainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.wantchatmainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantChatMainApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
